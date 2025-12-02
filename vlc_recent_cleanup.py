5343048edfd934abf06dc8beedf855cfd4361a6d
Jane Doe <denis.at.georgia@gmail.com>
2025-12-02 04:05:19 +0300

    add -v/-verbose option to print removed items

--- vlc_recent_cleanup.py
+++ vlc_recent_cleanup.py
@@ -23,8 +23,8 @@ MEDIA_DICT_KEY: Final = 'recentlyPlayedMedia'
 FILE_SCHEME: Final = 'file://'
 
 
-def drop_files_by_user_func(*, plist: dict, should_drop_func: ShouldDropFileFunc) -> None:
-    # process list
+def drop_files_by_user_func(*, plist: dict, should_drop_func: ShouldDropFileFunc, removed: Set[str]) -> None:
+    # process recently played list
     if MEDIA_LIST_KEY in plist:
         filenames = plist[MEDIA_LIST_KEY]
         for name in list(filenames):
@@ -32,17 +32,19 @@ def drop_files_by_user_func(*, plist: dict, should_drop_func: ShouldDropFileFunc
                 # list could contain duplicates
                 while name in filenames:
                     filenames.remove(name)
+                    removed.add(name)
 
-    # process dict with positions
+    # process dict with resume positions
     if MEDIA_DICT_KEY in plist:
         name2position = plist[MEDIA_DICT_KEY]
         keys = set(name2position.keys())
         for name in keys:
             if should_drop_func(name):
                 name2position.pop(name, None)
+                removed.add(name)
 
 
-def drop_files_by_ext(*, plist: dict, extensions: Set[str]) -> None:
+def drop_files_by_ext(*, plist: dict, extensions: Set[str], removed: Set[str]) -> None:
     extensions = {x.lower() for x in extensions}
     print('remove items with extensions:', repr(extensions))
 
@@ -56,7 +58,7 @@ def drop_files_by_ext(*, plist: dict, extensions: Set[str]) -> None:
             return False
         return ext in extensions
 
-    drop_files_by_user_func(plist=plist, should_drop_func=should_drop)
+    drop_files_by_user_func(plist=plist, should_drop_func=should_drop, removed=removed)
 
 
 def cleanup_dir(dirname: str) -> str:
@@ -65,7 +67,7 @@ def cleanup_dir(dirname: str) -> str:
     return dirname
 
 
-def drop_files_inside_dirs(*, plist: dict, exclude_dirs: Set[str]) -> None:
+def drop_files_inside_dirs(*, plist: dict, exclude_dirs: Set[str], removed: Set[str]) -> None:
     print('remove items inside subsirs:', repr(exclude_dirs))
     exclude_dirs = tuple(cleanup_dir(x) for x in exclude_dirs)
 
@@ -76,13 +78,14 @@ def drop_files_inside_dirs(*, plist: dict, exclude_dirs: Set[str]) -> None:
         filename = filename.lower()
         return filename.startswith(exclude_dirs)
 
-    drop_files_by_user_func(plist=plist, should_drop_func=should_drop)
+    drop_files_by_user_func(plist=plist, should_drop_func=should_drop, removed=removed)
 
 
 @dataclasses.dataclass(frozen=True)
 class Config:
     drop_exts: Set[str]
     drop_dirs: Set[str]
+    verbose: bool
 
     def __post_init__(self) -> None:
         if self.drop_exts:
@@ -108,8 +111,14 @@ def read_cli_config(args: List[str]) -> Config:
         dest='drop_dirs',
         help='remove files under specific directory, e.g. "~/tmp" (could be used many times)',
     )
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        action='store_true',
+        help='print removed items',
+    )
     result = parser.parse_args(args)
-    return Config(drop_exts=set(result.drop_exts), drop_dirs=set(result.drop_dirs))
+    return Config(drop_exts=set(result.drop_exts), drop_dirs=set(result.drop_dirs), verbose=result.verbose)
 
 
 def rename_file_to_backup(filename: str) -> None:
@@ -129,13 +138,22 @@ def main(args: List[str]) -> None:
     with open(plist_filename, 'rb') as inp:
         plist = plistlib.load(inp, fmt=plistlib.FMT_BINARY)
 
-    drop_files_by_ext(plist=plist, extensions=config.drop_exts)
-    drop_files_inside_dirs(plist=plist, exclude_dirs=config.drop_dirs)
+    removed: Set[str] = set()
+    drop_files_by_ext(plist=plist, extensions=config.drop_exts, removed=removed)
+    drop_files_inside_dirs(plist=plist, exclude_dirs=config.drop_dirs, removed=removed)
 
     rename_file_to_backup(plist_filename)
 
     with open(plist_filename, 'wb') as out:
         plistlib.dump(plist, out, fmt=plistlib.FMT_BINARY)
+
+    if config.verbose:
+        if removed:
+            print('removed items:')
+            for x in sorted(removed):
+                print(x)
+        else:
+            print('no items removed.')
 
 
 if __name__ == '__main__':
